ad49d0db371c140071671965cbb996a4354071c8
unaG <veraguo11@163.com>
2024-07-20 18:26:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ box_infos = std.detect('20240720175718.png')
 
 
 for box_info in box_infos['detected_texts']:
-    print(f"box_info: {box_info}")
+    # print(f"box_info: {box_info}")
     cropped_img = box_info['cropped_img']
     ocr_res = cn_ocr.ocr_for_single_line(cropped_img)
     print('ocr result: %s' % str(ocr_res))
