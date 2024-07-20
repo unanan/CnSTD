d67c744f96db6b1b819b04e8da23a0ab187585fd
unaG <veraguo11@163.com>
2024-07-20 18:26:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from cnstd import CnStd
 from cnocr import CnOcr
 import base64
@@ -14,22 +14,28 @@ def base64_to_pil(base64_str):
     return image
 
 
-@app.route('/ocr', method="POST")
+@app.route('/ocr', methods=["POST"])
 def ocr_process():
-    data = flask.get_data()
-    image_b64 = data["image"]
+    image_b64 = request.json.get("image")
     image_pil = base64_to_pil(image_b64)
 
     box_infos = cn_std.detect(image_pil)
 
+    results = []
     for box_info in box_infos['detected_texts']:
         cropped_img = box_info['cropped_img']
         ocr_res = cn_ocr.ocr_for_single_line(cropped_img)
         print('ocr result: %s' % str(ocr_res))
+        results.append({
+            "box": box_info["box"],
+            "text": ocr_res["text"],
+            "score": ocr_res["score"]
+        })
+    return results
 
 
 if __name__ == '__main__':
     cn_std = CnStd()
     cn_ocr = CnOcr()
 
-    app.run()
+    app.run(host="0.0.0.0", port=6006)
